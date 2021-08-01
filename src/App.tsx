d90c6806ab9bcf33ac6499ea7fbc61e40244baff
Atanas Ivaylov Laskov <latanas@gmail.com>
2021-08-01 21:43:27 +0100

    Add typescript to project.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let test: string = "AL";
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {test}
         </p>
         <a
           className="App-link"
