84891f756528ab587893fac846e126d744c7af8f
Atanas Laskov <latanas@gmail.com>
2023-11-28 11:10:02 +0000

    Remove placeholder HTML, change base path tocurrent app folder.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'town'`, () => {
+  it(`should have as title 'City @ Atanas Laskov'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('town');
+    expect(app.title).toEqual('City @ Atanas Laskov');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('town app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('City app is running!');
   });
 });
